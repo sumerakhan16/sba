8ea7cca96a771a95e8619e6ff2ee1ae49f1e205e
Sumera Khan <sumerakhan1698@gmail.com>
2021-02-16 16:48:44 -0600

    final changes

---
+++ script/script.js
@@ -0,0 +1,43 @@
+
+
+function menu(value) {
+    window.location.assign(`${value}.html`);
+}
+
+// carosuel //
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+//form validation for name in reserve
+function validation() {
+    var name = document.getElementById('name').value;
+    if(name == "" || /[^a-zA-Z]/.test(name) ){
+        alert("Please enter in a name!");
+    }
+    
+}
+
